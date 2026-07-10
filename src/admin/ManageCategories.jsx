0d38efb958fbf6f6e4f42ab741e8f4e3b85c1a26
badr <badrhanafi82@gmail.com>
2026-07-10 20:36:45 +0100

    test for useref import

--- src/admin/ManageCategories.jsx
+++ src/admin/ManageCategories.jsx
@@ -1,7 +1,7 @@
 // src/components/ManageCategories.jsx
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import { useRef } from "react";
 export default function ManageCategories() {
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(true);
